fa391efd95583badf78e10cb772c3e06197d7d89
Melody Wu <110306066@g.nccu.edu.tw>
2024-02-15 21:08:54 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className='h-full w-full flex flex-col gap-20 items-center justify-center'>
+      <h1 className='text-8xl font-bold'>Hello</h1>
+      <div className='flex flex-col gap-3 text-2xl'>
+        <span>choice 1</span>
+        <span>choice 1</span>
+        <span>choice 1</span>
+        <span>choice 1</span>
+      </div>
+    </div>
+  )
+}
+
+export default App
